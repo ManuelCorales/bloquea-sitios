4113e04816e3e108ea5f1c10d1a6155e32a05508
ManuCorales <manuelcorales0712@gmail.com>
2025-03-06 14:21:38 -0300

    some corrections

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # bloquea-sitios
-Abierto a mejoras, comentarios
-Extensión de chrome para blockear sitios web. Útil para no distraerse al usar Chrome
-Para eso hay que poner sus dominios en el archivo blacklist.js
+Abierto a mejoras, comentarios\
+Extensión de chrome para blockear sitios web. Útil para no distraerse al usar Chrome\
+Para eso hay que poner sus dominios en el archivo blacklist.js\
 Y después [agregar la extensión a chrome](https://developer.chrome.com/docs/extensions/get-started/tutorial/hello-world?hl=es-419#load-unpacked)
 
-
---
-Open to improvements or comments
-Chrome add-on for blocking websites. Useful for concentrating while using Chrome
-Need to modify blacklist.js to add the blocked domains
+Open to improvements or comments\
+Chrome add-on for blocking websites. Useful for concentrating while using Chrome\
+Need to modify blacklist.js to add the blocked domains\
 And then [add extension to chrome](https://developer.chrome.com/docs/extensions/get-started/tutorial/hello-world?hl=es-419#load-unpacked)
--- src/blocker.js
+++ src/blocker.js
@@ -1,5 +1,15 @@
 import blacklist from '../blacklist.js';
 
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    const url = new URL(tab.url);
+
+    if(!isSiteAllowed(url)) {
+        chrome.tabs.remove(tabId);
+    }
+
+});
+
+
 function isSiteAllowed(url) {
     for(let blockedSite of blacklist) {
         if(blockedSite.hostname == url.hostname) {
@@ -13,11 +23,12 @@ function isSiteAllowed(url) {
     return true;
 }
 
+
 function hasAllowedPath(blockedSite, url) {
     for(let allowedPath of blockedSite.allowedPaths) {
         let parsedPath = allowedPath;
 
-        // I asume that there those are the last two characters of the string and are no others like those
+        // I assume that those are the last two characters of the string and are no others like them
         if(allowedPath.includes("/*")) {
             parsedPath = parsedPath.slice(0, parsedPath.length - 1);
         }
@@ -25,13 +36,3 @@ function hasAllowedPath(blockedSite, url) {
         return url.pathname.includes(parsedPath);
     }
 }
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    const url = new URL(tab.url);
-
-    if(!isSiteAllowed(url)) {
-        chrome.tabs.remove(tabId);
-    }
-
-});
-
